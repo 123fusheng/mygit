421bc4309409a01238f46e04b25af29e977bc67b
zmtgit <1428872869@qq.com>
2022-08-21 10:49:06 +0800

    feature:mul
    feature:div

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int add(int a,int b){
-	return a*b;
+	return a+b+1;
 }
 int del(int a, int b){
-i	return a/b;
+i	return a-b;
 }
+int mul (int a,int c){
+	return a*c;
+}
+double div(int a,int b){
+	if(b==0) retur 0xffffff;
+	return a/b;
+}
+
 int main(){
 	printf("hello world\n");
 	return 0;
